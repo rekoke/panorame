2df0cc9bdac22903e5b6d49c659a0275ea035643
koke <rekoke92@gmail.com>
2023-08-26 20:42:17 +0200

    pricing tables mobile

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
 import PriceTable from "./priceTable/PriceTable";
 import styles from "../styles/Pricing.module.css";
 import { Open_Sans } from "@next/font/google";
 import ContainerWrapper from "./ContainerWrapper";
+import { useInView } from "react-intersection-observer";
 
 const open = Open_Sans({ subsets: ["latin"] });
 
 const Pricing = () => {
+  const { ref, inView, entry } = useInView({
+    threshold: 0.5,
+  });
+
+  const scroll = inView ? "" : styles.secondSlide;
+
   return (
     <ContainerWrapper
       sectionTitle="Pricing"
       className={styles.pricingContainer}
     >
-      <>
+      <div className={styles.pricingWrapper}>
         <div className={styles.pricingContainerTables}>
           <ul>
-            <li>
+            <li ref={ref}>
               <PriceTable type="small" size={300} price={6000000} />
             </li>
             <li>
@@ -22,6 +30,9 @@ const Pricing = () => {
             </li>
           </ul>
         </div>
+        <div
+          className={`${styles.scrollBarPrices} ${styles.onlyMobile} ${scroll}`}
+        ></div>
         <p className={`${open.className} ${styles.discount}`}>
           2nd year renovation has a <span>30%</span> discount
         </p>
@@ -29,7 +40,7 @@ const Pricing = () => {
           <span>*</span>Properties larger than 700㎡ or multiple
           space/properties will be discussed upon consultation.
         </p>
-      </>
+      </div>
     </ContainerWrapper>
   );
 };
